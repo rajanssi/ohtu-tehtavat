b0c89e947ad8961de40d854d776c5a0d9eb370d7
Anssi Rajala <ajrajal4@gmail.com>
2021-11-19 17:23:35 +0200

    R4 - R6 done

---
+++ viikko3/login-robot/src/services/user_service.py
@@ -0,0 +1,46 @@
+from entities.user import User
+import re
+import sys, pdb
+
+class UserInputError(Exception):
+    pass
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self._user_repository = user_repository
+
+    def check_credentials(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+
+        user = self._user_repository.find_by_username(username)
+
+        if not user or user.password != password:
+            raise AuthenticationError("Invalid username or password")
+
+        return user
+
+    def create_user(self, username, password):
+        self.validate(username, password)
+
+        user = self._user_repository.create(
+            User(username, password)
+        )
+
+        return user
+
+    def validate(self, username, password):
+        if not username or not password:
+            raise UserInputError("Username and password are required")
+        if len(username) < 3:
+            raise UserInputError("Username too short")
+        if len(password) < 8:
+            raise UserInputError("Password too short")
+        if not re.match("^[a-zA-Z]+[0-9]+$", password):
+            raise UserInputError("Password must contain letters and numbers")
+
