7628eaed5fd533cb10bc487cbd08c23c0a047328
Anssi Rajala <ajrajal4@gmail.com>
2021-11-25 13:57:58 +0200

    after adding one item to the basket, basket will contain one item

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -47,3 +47,21 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito2)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisamisen_jalkeen_korin_hinta_on_niiden_hintojen_summa(self):
+        maito1 = Tuote("Maito", 3)
+        maito2 = Tuote("Maito", 3)
+        
+        self.kori.lisaa_tuote(maito1)
+        self.kori.lisaa_tuote(maito2)
+
+        self.assertEqual(self.kori.hinta(), 6)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+ 
+        ostokset = self.kori.ostokset()
+ 
+        # testaa että metodin palauttaman listan pituus 1
+        self.assertEqual(len(ostokset), 1)
