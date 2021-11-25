74e5cd999d9171f18755513d11e772d37e93180c
Anssi Rajala <ajrajal4@gmail.com>
2021-11-25 13:52:41 +0200

    after adding two different items to a basket, basket price will be the sum of all items in the basket

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -30,3 +30,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(murot)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        murot = Tuote("Murot", 5) 
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(murot)
+        
+        self.assertEqual(self.kori.hinta(), 8)
