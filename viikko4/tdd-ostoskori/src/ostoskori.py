fa0eccf65393d5caf4e1e6a6cc75e3b7a2b2bdda
Anssi Rajala <ajrajal4@gmail.com>
2021-11-25 13:47:44 +0200

    basket price is the sum of all product prices in the basket

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -13,7 +13,8 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        hinnat = map(lambda t: t.hinta(), self.sisalto)
+        return sum(hinnat) 
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
